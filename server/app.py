749a036a70bdbf07a52f2fb4fe640a3a0631c0fb
LeightonSkervin <lskervin23@gmail.com>
2024-03-30 00:34:21 -0400

    lab complete

--- server/app.py
+++ server/app.py
@@ -1,8 +1,46 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return f'{parameter}'
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    # Generate numbers separated by newline characters
+    numbers = '\n'.join(str(i) for i in range(parameter))
+    return numbers + '\n' # Return numbers separated by newline characters
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    result = None
+    
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        # Check for division by zero
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return "Error: Division by zero"
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        raise ValueError("Error: Invalid operation")
+    
+    return str(result)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
